0168c32e99f6796aef69554ca97e335fe0e765a0
serii <seriiburduja@gmail.com>
2023-11-15 14:04:35 +0200

    feat: sublist

--- src/components/list/ListItem.vue
+++ src/components/list/ListItem.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import {defineProps, PropType} from "vue";
+import {defineProps, onMounted, PropType, ref} from "vue";
 import IconPlay from "../icons/IconPlay.vue";
 import IconEdit from "../icons/IconEdit.vue";
 import IconDelete from "../icons/IconDelete.vue";
 import {IListItem} from "../../interfaces/list/IList";
 import SubList from "./SubList.vue";
+
+const input_ref = ref<HTMLInputElement>();
 
 const props = defineProps({
   title: {
@@ -16,15 +18,34 @@ const props = defineProps({
     required: true,
   }
 });
+const value = ref("");
+const disabled = ref(true);
+function toggleDisabled() {
+  disabled.value = !disabled.value;
+  if (disabled.value) {
+    input_ref.value?.blur();
+  } else {
+    input_ref.value?.focus();
+  }
+}
+onMounted(() => {
+  value.value = props.title;
+});
 </script>
 
 <template>
   <li class="list__item">
-    <div class="list__title">{{ title }}</div>
+    <input
+        :ref="input_ref"
+        type="text"
+        v-model="value"
+        class="list__input"
+        :class="{'active': !disabled}"
+        :disabled="disabled">
     <div class="list__play">
       <IconPlay/>
     </div>
-    <div class="list__edit">
+    <div class="list__edit" @click="toggleDisabled">
       <IconEdit/>
     </div>
     <div class="list__delete">
