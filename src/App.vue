6059d89a6c705a1dbf3331dc7b38f2fbba98d94e
serii <seriiburduja@gmail.com>
2023-11-15 18:44:46 +0200

    feat: import export

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import {nextTick, onMounted, onUpdated, ref, watch} from "vue";
+import {onMounted, ref, watch} from "vue";
 import ListItem from "./components/list/ListItem.vue";
 import {IList} from "./interfaces/list/IList";
 import useGetFromLocalStorage from "./hooks/useGetFromLocalStorage";
 import useAddToLocalStorage from "./hooks/useAddToLocalStorage";
+import useChangeLocalStorage from "./hooks/useChangeLocalStorage";
+import useClearLocalStorage from "./hooks/useClearLocalStorage";
 
 const input_ref = ref<HTMLInputElement>();
+const file_ref = ref<HTMLInputElement>();
 const title = ref("");
 
 const items = ref<IList[]>();
@@ -17,15 +20,18 @@ watch(search, (value) => {
   if (value === "") {
     filtered.value = items.value;
   } else {
-    filtered.value = items.value.filter((item) => {
+    const filtered_lc = items.value.filter((item) => {
       return item.title.toLowerCase().includes(value.toLowerCase());
     });
+    if(filtered_lc.length > 0){
+      filtered.value = filtered_lc;
+    }
   }
 });
 
 async function onSubmit() {
   const tabs = [];
-  await chrome.windows.getCurrent({ populate: true }, function (window) {
+  await chrome.windows.getCurrent({populate: true}, function (window) {
     window.tabs.forEach(function (tab) {
       tabs.push(tab);
     });
@@ -56,7 +62,48 @@ function updateFromLocalStorage() {
     items.value = all_tabs;
     items.value = items.value.reverse();
     filtered.value = items.value;
+  }else{
+    items.value = [];
+    filtered.value = [];
   }
+}
+
+function importAll() {
+  const file = file_ref.value.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const fileContents = e.target.result;
+      const tabs = JSON.parse(fileContents);
+      console.log(tabs, 'tabs')
+      useChangeLocalStorage(tabs);
+      updateFromLocalStorage();
+    };
+    const result = reader.readAsText(file);
+  }
+}
+
+function exportAll() {
+  const all_tabs = useGetFromLocalStorage();
+  const blob = new Blob([JSON.stringify(all_tabs)], {type: "application/json"});
+  const url = URL.createObjectURL(blob);
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const file_name = `${hours}_${minutes}_${day}-${month}-${year}.json`;
+  chrome.downloads.download({
+    url: url,
+    filename: file_name,
+    saveAs: true
+  });
+}
+
+function clearAll(){
+  useClearLocalStorage();
+  updateFromLocalStorage();
 }
 
 onMounted(() => {
@@ -69,9 +116,15 @@ onMounted(() => {
     <header class="popup__header">
       <h2 class="popup__title">Create new project</h2>
       <input :ref="input_ref" class="popup__input" type="text" v-model="title">
+      <div class="btn popup__import">
+        <span>Import</span>
+        <input @change="importAll" ref="file_ref" type="file" id="fileInput"/>
+      </div>
+      <button @click="exportAll" class="btn btn--success">Export</button>
+      <button @click="clearAll" class="btn btn--error">Clear</button>
       <button @click="onSubmit" :disabled="title === ''" class="btn">Create</button>
     </header>
-    <input type="text" placeholder="Search project" class="popup__search" v-model="search">
+    <input v-if="filtered && filtered.length > 0" type="text" placeholder="Search project" class="popup__search" v-model="search">
     <ul v-if="items && items.length > 0" class="list">
       <ListItem
           v-for="item in filtered"
--- src/hooks/useChangeLocalStorage.ts
+++ src/hooks/useChangeLocalStorage.ts
@@ -1,6 +1,3 @@
 export default function useChangeLocalStorage(value) {
-    const lc = localStorage.getItem('all_tabs');
-    if (lc) {
-        localStorage.setItem('all_tabs', JSON.stringify(value));
-    }
+    localStorage.setItem('all_tabs', JSON.stringify(value));
 }
