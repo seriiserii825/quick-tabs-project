c81167e9a0c1520cf8242503fd74b083fa72016f
serii <seriiburduja@gmail.com>
2023-11-15 13:11:31 +0200

    feat: start popup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
+const title = ref('');
 </script>
 
 <template>
   <div class="popup">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <header class="popup__header">
+      <h2 class="popup__title">Create new tab</h2>
+      <input class="popup__input" type="text" v-model="title">
+    </header>
+    <ul class="list">
+      <li class="list__item">
+        <div class="list__title">New site</div>
+        <div class="list__edit"></div>
+        <div class="list__delete"></div>
+        <ul class="sublist">
+          <li class="sublist__item"></li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style>
 #app {
-  width: 1280px;
-  background-color: green;
-}
-.popup  {
+  width: 780px;
 }
 </style>
