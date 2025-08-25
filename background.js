a2eb5375ded3bd67b06e5a8ae4ad859c0698c4d1
serii <seriiburduja@gmail.com>
2025-08-25 20:59:31 +0300

    feat: new files

--- background.js
+++ background.js
@@ -1,67 +1,7 @@
 // script start after extension install
 chrome.runtime.onInstalled.addListener(async () => {
-    // alert("extension installed");
     console.log("extension installed");
 });
-chrome.commands.onCommand.addListener((command) => {
-    // tab is active tab
-    chrome.tabs.update({}, async (tab) => {
-        if (command == "tabs-to-buffer") {
-            console.log("extension command: tabs-to-buffer");
-            // let url = chrome.runtime.getURL("html/question.html");
-            // // open this url in new tab
-            // let tab = await chrome.tabs.create({url});
-        }
-        // } else if (command == "clear-local-storage") {
-        //     chrome.storage.local.clear(() => {
-        //         console.log('Everything was removed');
-        //         chrome.notifications.create(
-        //             "notification1",
-        //             {
-        //                 type: "basic",
-        //                 iconUrl: "/icons/alert.png",
-        //                 title: "Local storage",
-        //                 message: "Everything was removed",
-        //                 buttons: [{title: "Thanks"}],
-        //                 priority: 0,
-        //             },
-        //             (e) => {
-        //                 console.log("Last error:", chrome.runtime.lastError);
-        //             }
-        //         );
-        //     });
-        // }
-    });
-})
-;
-//notifications
-chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log("onAlarm", alarm);
-    chrome.notifications.create(
-        "notification1",
-        {
-            type: "basic",
-            iconUrl: "/icons/alert.png",
-            title: "Напоминашка",
-            message: "Ахтунг! Пришло время напомнить!",
-            buttons: [{title: "Спасибо"}],
-            priority: 0,
-        },
-        (e) => {
-            console.log("Last error:", chrome.runtime.lastError);
-        }
-    );
-});
-chrome.notifications.onButtonClicked.addListener(
-    async (notificationId, buttonIndex) => {
-        console.log({notificationId, buttonIndex});
-    }
-);
-chrome.notifications.onClicked.addListener(async (notificationId) => {
-    chrome.storage.sync.get(["timer"], (result) => {
-        if (result.timer) {
-            chrome.alarms.create({delayInMinutes: result.timer});
-        }
-    });
-    chrome.notifications.clear(notificationId);
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.create({ url: chrome.runtime.getURL("index.html") });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import {crx} from '@crxjs/vite-plugin'
-import manifest from './manifest.json' // Node 14 & 16
-// import manifest from './public/manifest.json' assert {type: 'json'} // Node >=17
+// vite.config.ts
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        crx({manifest}),
-    ],
-})
+  base: "./",                 // 👈 ensures built asset URLs are relative
+  plugins: [vue(), crx({ manifest })],
+});
